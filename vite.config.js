e4254e8dfe53f4b8a98a6789134de85099e4f720
Numaan Ali <alinumaan35@gmail.com>
2025-10-22 06:34:57 -0700

    1111

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/auto-repair-website/', // 👈 same as your repo name
-})
+  base: "/", // ✅ ye line zaroor honi chahiye
+});
